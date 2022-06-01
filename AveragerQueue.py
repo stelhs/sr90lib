9f6224aa338962a99e719dcbe4087c0d8e5f133c
Michail Kurochkin <stelhs@ya.ru>
2022-06-02 02:18:20 +0300

    fixed AveragerQueue

--- AveragerQueue.py
+++ AveragerQueue.py
@@ -1,19 +1,32 @@
+import threading
+
+
+class AveragerQueueEmptyError(Exception):
+    pass
+
 
 class AveragerQueue():
-    def __init__(s, size=0):
+    def __init__(s, size=0, queue=[]):
         s.size = size
+        s.lock = threading.Lock()
         s.clear()
+        s._q = queue
 
     def push(s, val):
-        s._q.append(val)
-        if s.size and (len(s._q) > s.size):
-            s._q = s._q[1:]
+        with s.lock:
+            s._q.append(val)
+            if s.size and (len(s._q) > s.size):
+                s._q = s._q[1:]
 
 
     def round(s):
-        s._q.sort()
-        return s._q[int((len(s._q) / 2) - 1)]
+        with s.lock:
+            s._q.sort()
+            if not len(s._q):
+                raise AveragerQueueEmptyError()
+            return s._q[int((len(s._q) / 2) - 1)]
 
 
     def clear(s):
-        s._q = []
+        with s.lock:
+            s._q = []
